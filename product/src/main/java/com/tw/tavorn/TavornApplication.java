0c2a4770abc3c22af6c093492da0ef0443029d78
Tongchai Paocharoen <tongpao@ibm.th.com>
2019-07-28 07:17:48 +0700

    new product application

---
+++ product/src/main/java/com/tw/tavorn/TavornApplication.java
@@ -0,0 +1,13 @@
+package com.tw.tavorn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TavornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TavornApplication.class, args);
+	}
+
+}
